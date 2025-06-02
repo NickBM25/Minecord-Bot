9be2e3952b669c6ef6ef04b42066aba6bd2821e3
Nicolas Macedo <nickbmcoder@gmail.com>
2025-06-02 09:55:41 -0300

    Welcome message added.

--- src/main.py
+++ src/main.py
@@ -2,15 +2,22 @@ import os
 import discord
 from dotenv import load_dotenv
 
+#token, thats it.
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-bot = discord.Bot()
+#setup intents and instantiate the bot
+intents = discord.Intents.default()
+intents.members = True
+bot = discord.Bot(intents=intents)
 
+#print in console when the bot is ready.
 @bot.event
 async def on_ready():
     print(f"Bot {bot.user} ligado - BEEP BOOP")
 
-bot.load_extension('cogs.hello')
+#load cogs
+bot.load_extension('cogs.greetings')
 
-bot.run(TOKEN)
+
+bot.run(TOKEN) #runs the bot, hopefully.
